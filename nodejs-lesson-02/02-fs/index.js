5ac4a2e90a459102fe6682ec810d7f63fcf634d3
samuel <samuellasmi.3@gmail.com>
2021-02-22 18:23:12 +0200

    node: lesson 2 - 2 test

---
+++ nodejs-lesson-02/02-fs/index.js
@@ -0,0 +1,20 @@
+// const EventEmitter = require('events');
+
+// class MyClass extends EventEmitter {
+//     call(){
+//         this.on('samuel',this.message);
+//         this.on('dvori',this.newmessage)
+//         this.emit('samuel')
+//     }
+//     message(){
+//     console.log('work work work ');
+
+//     }
+//     newmessage(){
+//         setTimeout(() => {
+//             console.log('running after 2 secend');
+//         }, 2000);
+//     }
+//  };
+// const hello = new MyClass();
+// hello.call()
