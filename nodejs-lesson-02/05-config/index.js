54a93dc8dc4d6318989ec8aebd7bfb87087cd185
Yoni vidal <yonivid@gmail.com>
2021-02-22 21:35:08 +0200

    class last hour

---
+++ nodejs-lesson-02/05-config/index.js
@@ -0,0 +1,14 @@
+/*
+    node config;
+*/
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === "production") {
+    global.config = require("./prod.json");
+
+} else {
+    global.config = require("./dev.json");
+};
+
+console.log(`Database Name: ${global.config.database.name}`);
