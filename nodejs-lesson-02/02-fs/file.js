75759e57fd68ebce00af8bb4bbe1cb0242f79820
Yoni vidal <yonivid@gmail.com>
2021-02-22 19:04:47 +0200

    exc 1

---
+++ nodejs-lesson-02/02-fs/file.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+const appendData = (filename, data) => {
+    fs.appendFileSync(filename, `${data}`)
+
+
+}
+
+module.exports = appendData;
--- nodejs-lesson-02/02-fs/index.js
+++ nodejs-lesson-02/02-fs/index.js
@@ -1,3 +1,13 @@
-test('should ', () => {
+const fs = require('fs')
+const appendData = require('./file')
 
-})
+
+const filename = './example.txt'
+// fs.writeFileSync(filename, 'hello world')
+// fs.appendFileSync(filename, '\n bye bye')
+// fs.appendFileSync(filename, '\n bye bye man')
+
+
+appendData('./example.txt', '\n its working!!')
+const data = fs.readFileSync(filename, 'utf-8')
+console.log(data);
