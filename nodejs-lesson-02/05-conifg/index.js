51e43fc664b0ce01456d1ff08caff81ed6b7d090
shaked <shakedteler@gmail.com>
2021-02-22 21:41:48 +0200

    new

--- nodejs-lesson-02/05-conifg/index.js
+++ nodejs-lesson-02/05-conifg/index.js
@@ -1,5 +1,4 @@
-console.log("process.env.NODE_ENV" , process.env.NODE_ENV
-)
+console.log("process.env.NODE_ENV" , process.env.NODE_ENV)
 
 // if(process.env.NODE_ENV === "production"){
 //     global.config = require("./prod.json");
