3497fcbdedd05a938ac012a7c5a530c3f88d12ec
Hen <hen.haim222@gmail.com>
2021-02-22 21:36:35 +0200

    checking 03

--- nodejs-lesson-02/04-globals/index.js
+++ nodejs-lesson-02/04-globals/index.js
@@ -7,5 +7,25 @@ module.exports = {
     funcyey : () =>{console.log("yey")},
     name:"Moishe"
 }
+/* ------------------------------ after module; ------------------------------ */
 
 console.log(module);
+
+/* ------------------------ __filename and __dirname; ----------------------- */
+console.log("\n****************\n __dirname Add __filename \n****************\n");
+console.log("__dirname", __dirname);
+console.log("__filename", __filename);
+
+/* --------------------------------- global --------------------------------- */
+console.log("\n*******************\n Global \n*******************\n");
+global.nameBlabla = "Moishe Ufnik";
+const anotherModule = require("./another-modul");
+console.log(module);
+
+/* --------------------------------- process --------------------------------- */
+
+console.log("\n*******************\n process \n*******************\n");
+console.log(process);
+
+
+
---
+++ nodejs-lesson-02/05-config/index.js
@@ -0,0 +1,8 @@
+if (process.env.NODE_ENV === "production") {
+    global.config = require("./prod.json");
+
+} else {
+    global.config = require("./dev.json");
+};
+
+console.log(`Database Name: ${global.config.database.name}`);
