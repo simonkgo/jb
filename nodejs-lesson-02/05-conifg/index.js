66126da56e0b05c376cf0e24f377efc509882d29
shaked <shakedteler@gmail.com>
2021-02-22 21:34:41 +0200

    simon check

---
+++ nodejs-lesson-02/05-conifg/index.js
@@ -0,0 +1,10 @@
+console.log("process.env.NODE_ENV" , process.env.NODE_ENV
+)
+
+// if(process.env.NODE_ENV === "production"){
+//     global.config = require("./prod.json");
+// }else {
+//     global.config = require("./dev.json");
+// };
+
+// console.log(`DataBase Name : ${global.config.database.name}`)
