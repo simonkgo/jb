c2ac301ae68e660900cb62774f07311761d002ba
Yoni vidal <yonivid@gmail.com>
2021-02-23 20:40:58 +0200

    simone code

--- nodejs-lesson-02/05-config/index.js
+++ nodejs-lesson-02/05-config/index.js
@@ -1,7 +1,8 @@
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     global.config = require("./prod.json");
-}else {
+} else {
     global.config = require("./dev.json");
 };
 
+console.log(`DataBase Name : ${global.config.database.name}`)
 console.log(`DataBase Name : ${global.config.database.name}`)
