a7230bb8a5104812631e9f37d63f024356b3e4ad
Hen <hen.haim222@gmail.com>
2021-03-06 21:04:49 +0200

    adding home work

--- nodejs-lesson-04/02-rest/index.js
+++ nodejs-lesson-04/02-rest/index.js
@@ -3,7 +3,7 @@ const books = [
     { "id": 2, "name": "JS", "author": "Kipi" },
     { "id": 3, "name": "PHP", "author": "Ugi" },
 ];
-
+// {"id": 4, "name": "C sharp", "author": "lili"}
 const express = require ("express");
 
 const app = express()
@@ -12,7 +12,6 @@ app.use(express.json());
 
 //GET all books//
 app.get("/api/books", (request, response)=>{
-    // let another = { "id": 4, "name": "C`", "author": "lili"};
     response.json(books);  
 
 });
@@ -41,11 +40,35 @@ app.put("/api/books/:bookId", (request, response)=>{
     let index = books.findIndex(myBook => myBook.id===id);
 
     books[index] = book;
-    
+
     response.json(books);
 });
 
-// {"id": 4, "name": "C sharp", "author": "lili"}
+//DELETE one books//
+app.delete("/api/books/:bookId", (request, response)=>{
+    const id = +request.params.bookId;
+
+    let index = books.findIndex(myBook => myBook.id===id);
+
+    books.splice(index,1);
+    response.json(books);
+});
+
+//PATCH one books//
+app.patch("/api/books/:bookId", (request, response)=>{
+    const id = +request.params.bookId;
+    let book = request.body;
+    let index = books.findIndex(myBook => myBook.id===id);
+
+    for (let key in books[index]){
+
+        for (let key2 in book){
+            if(key2===key){books[index][key] = book[key2]}
+        }
+    }
+    response.json(books);
+});
+
 
 //listener//
 app.listen("3000", () =>{
---
+++ nodejs-lesson-04/03-rest-in-layers/index.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+
+export class Server{
+    public app:express.Application;
+    constructor(){
+        this.activate();
+    }
+    activate (){
+        this.app = express();
+        this.app.use(express.json())
+        this.app.listen("3000", () =>console.log("Server listening"))
+    }
+    
+
+}
+new Server();
