3a7d43d75594f3de5fba890e9be4a0f3fe6eb971
Maria <masha.elgart@gmail.com>
2021-03-06 15:43:21 +0200

    add folder nodejs-lesson-03

--- nodejs-lesson-02/01-home-work/color-printer.js
+++
@@ -1,9 +0,0 @@
-const colors = require("colors");
-
-function colorPrinter(arr) {
-    for(const item of arr) {
-        console.log(colors.random(item.toString()));
-    }
-}
-
-module.exports = colorPrinter;
