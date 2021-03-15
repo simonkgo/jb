800cc9cdd0d8ae9740740186d6167e6813a390ed
simon <s@gmail.com>
2021-03-15 16:50:08 +0200

    nodejs: lesson06 - start;

---
+++ nodejs-lesson-06/01-rest-in-layers-basic/index.ts
@@ -0,0 +1,70 @@
+/*
+    ?מה בונים
+    שרת רסט עם אסקפרס ושכבות
+    שמקבל בקשות מקליינט
+    ומעדכן את האובייקט של המוצרים בהתאם לחוקים של רסט
+    משמע - get/put/post/delete/patch
+    ושלחיה לנתיב המתאים 
+    get     /products     בקשה לקבל את רשימת כל המוצרים
+    post    /products    הוספה של מוצר חדש
+    put     /products/:id עדכון כל המוצר
+    patch   /products/:id עדכון של חלק מהמוצר
+    delete  /products/:id מחיקה של המוצר
+
+    controller  - מראה את סוגי האייפי איי כרגע רק רסט בעתיד יכול להיות דברים נוספים
+    service     - מכיל את הלוגיקה של האפליקציה ושם רוב העבודה
+    respository - מחובר לקבצים במקרה שלנו בעתיד לדאטה בייס והתפקיד זה לקבל לקרוא מהקובץ או לכתוב אליו וזהו 
+
+    ?מה יש לנו
+    database
+    products.json - קובץ שמכיל מערך של מוצרים
+
+    products תיקיה של 
+    product.ts - האובייקט עצמו שנשתשמ בו כטייפ ובהמשך נוסיף ולידציה
+    product-controller.ts - יבנה את הנתיבים ויפעיל פונקציה מתאימה
+    product-service.ts - יעשה את רוב העבודה של עדכונים הוספות וכו
+    product-repository.ts - products.json כותב וקורא מקובץ 
+
+    index.ts - products קובץ ראשי שנפעיל אותו והוא יאתחל את אקספרס ויחבר את  
+
+    tsconfig.ts - אחראי על הגדרות של טייפסקריפט
+ */
+
+import * as express from 'express';
+import { ProductsController } from './products/products-controller';
+
+export default class MyGreateRestAPIServer {
+    // הגדרה של אובייקט אקספרס;
+    //---;
+    public app: express.Application;
+
+    //products אתחול של אובייקט מתוך הקלאס שיצרנו שאחראי על בניית הנתיבים עבור;
+    //---;
+    private productController: ProductsController;
+
+    //הבנאי - ירוץ ראשון כאשר נאתחל את הקלאס הראשי של האפליקציה;
+    //---;
+    constructor() {
+        this.activate();
+    };
+
+    //פונקציה שהבנאי יריץ;
+    //---;
+    activate() {
+        //איתחול של אספרס והצבתו במאפיין;
+        this.app = express();
+
+        //רושים של מידלוור כללי שמאפשר שליחה של גייסון בגוף הבקשות;
+        this.app.use(express.json());
+
+        //רישום הנתיבים של המוצרים לכתובת הבאה;
+        this.app.use("/api/v1", new ProductsController().router); // /api/v1/products
+        //this.app.use("/api/v1", new MessagesController().router); // /api/v1/message
+
+        //פונקציה שתתחבר לפורט ותאפשר לקליינטים לפנות תוכנת שרת שאנחנו בונים;        
+        this.app.listen(3000, () => console.log("listen on port 3000"));
+    };
+};
+
+//בשביל לאתחל את קלאס השרת new שימוש במילה;
+new MyGreateRestAPIServer();
---
+++ nodejs-lesson-06/01-rest-in-layers-basic/js/products/products-service.js
@@ -0,0 +1,164 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var products_repository_1 = require("./products-repository");
+var ProductsService = /** @class */ (function () {
+    function ProductsService() {
+    }
+    ;
+    //פונקציה שמחזירה את מערך המוצרים מהריפוזיטורי;
+    //---;
+    ProductsService.prototype.all = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var products;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, products_repository_1.default.getAll()];
+                    case 1:
+                        products = _a.sent();
+                        //להחזיר רשימת מוצרים;
+                        return [2 /*return*/, products];
+                }
+            });
+        });
+    };
+    ;
+    //id פונקציה שתחזיר מוצר אחד לפי;
+    //---;
+    ProductsService.prototype.getOne = function (id) {
+        return __awaiter(this, void 0, void 0, function () {
+            var products, product;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, products_repository_1.default.getAll()];
+                    case 1:
+                        products = _a.sent();
+                        product = products.find(function (p) { return p.id === id; });
+                        //להחזיר רשימת מוצרים;
+                        return [2 /*return*/, product];
+                }
+            });
+        });
+    };
+    ;
+    //פונקציה שתוסיף מוצר חדש;
+    //---;
+    ProductsService.prototype.post = function (product) {
+        return __awaiter(this, void 0, void 0, function () {
+            var products;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, products_repository_1.default.getAll()];
+                    case 1:
+                        products = _a.sent();
+                        //תיתן לי את האובייקט האחרון במערך ותוסיף לאידי שלו 1;
+                        product.id = products[products.length - 1].id + 1;
+                        //הוספה של המוצר למערך המוצרים;
+                        products.push(product);
+                        //שמירת המערך בחזרה לקובץ אחרי ההוספה של המוצר החדש;
+                        return [4 /*yield*/, products_repository_1.default.saveAll(products)];
+                    case 2:
+                        //שמירת המערך בחזרה לקובץ אחרי ההוספה של המוצר החדש;
+                        _a.sent();
+                        //החזרת המוצר בחזרה ללקוח;
+                        return [2 /*return*/, product];
+                }
+            });
+        });
+    };
+    ;
+    //פונקציה שמשנה את האובייקט במלואו;
+    //---;
+    ProductsService.prototype.put = function (product) {
+        return __awaiter(this, void 0, void 0, function () {
+            var products, index;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, products_repository_1.default.getAll()];
+                    case 1:
+                        products = _a.sent();
+                        index = products.findIndex(function (p) { return p.id === product.id; });
+                        //החלפה של המוצר במערך במוצר החדש שנשלח מהקליינט;
+                        products[index] = product;
+                        //לשמור בקובץ את המערך המעודכן;
+                        return [4 /*yield*/, products_repository_1.default.saveAll(products)];
+                    case 2:
+                        //לשמור בקובץ את המערך המעודכן;
+                        _a.sent();
+                        //החזרת המוצר בחזרה ללקוח;
+                        return [2 /*return*/, product];
+                }
+            });
+        });
+    };
+    ;
+    //פונקציה שמשנה את האובייקט חלקית;
+    //---;
+    ProductsService.prototype.patch = function (product) {
+        return __awaiter(this, void 0, void 0, function () {
+            var products, productToFetch, key;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, products_repository_1.default.getAll()];
+                    case 1:
+                        products = _a.sent();
+                        productToFetch = products.find(function (p) { return p.id === product.id; });
+                        //ריצה על מפתחות של המוצר שהקליינט שלח ועדכון של המוצר במערך;
+                        for (key in product) {
+                            if (key in productToFetch) {
+                                productToFetch[key] = product[key];
+                            }
+                            ;
+                        }
+                        ;
+                        //שמירה בחזרה במערך בקובץ אחרי העדכון;
+                        return [4 /*yield*/, products_repository_1.default.saveAll(products)];
+                    case 2:
+                        //שמירה בחזרה במערך בקובץ אחרי העדכון;
+                        _a.sent();
+                        //החזרת המוצר בחזרה ללקוח;
+                        return [2 /*return*/, product];
+                }
+            });
+        });
+    };
+    ;
+    return ProductsService;
+}());
+;
+exports.default = new ProductsService();
