d14a31ceffdcb3d53510c5569774537f6d154026
Your Name <inbal@goldblatt.in>
2021-03-22 19:04:22 +0200

    recap class - middleware + route

---
+++ nodejs-lesson-07/01-recap/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const routeRecap = () => {
+    const app = express();
+
+    app.get("/api/v1/products", (req, res, next) => {
+        res.json("yey");
+    });
+
+    app.listen(3000, () => console.log("Listen on ports 3000"));
+};
+routeRecap();
+
+const middlewareRecap1 = () => {
+    const app = express();
+
+// Middleware before route
+    app.use((req,res,next) => {
+        console.log("First Step: Middleware");
+        next();
+    });
+
+    app.get("/api/v1/products", (req, res, next) => {
+        console.log("Second Step: Route - /api/v1/products");
+        res.json("yey");
+    });
+
+    app.listen(3001, () => console.log("Listen on ports 3001"));
+};
+middlewareRecap1();
+
+middlewareRecap2 = () => {
+    const app = express();
+    // route before middleware
+    app.get("/api/v1/products", (req, res, next) => {
+            console.log("First Step: Route - /api/v1/products");
+            next();
+        });
+
+    app.use((req, res, next) => {
+        console.log("Second Step: Middleware");
+        res.json("yey")
+    });
+
+    app.listen(3002, () => console.log("Listen on ports 3002"));
+};
+middlewareRecap2();
+
+const middlewareRecap3 = () => {
+    const app = express();
+
+    app.get("/api/v1/products", (req, res, next) => {
+        console.log("First Stop: route /api/v1/products");
+        next() //מעבר למידלוור למטה שמוגדר עם 3 ארגומנטים
+        // next("Oppso..."); //מעבר למידלוור למטה שמוגדר עם 4 ארגומנטים
+    });
+
+    // //מידלוור עם 3 ארומנטים;
+    app.use((req, res, next) => {
+        console.log("Second Stop: middleware with 3 arguments");
+        res.json("ok");
+    });
+
+    //מידלוור עם 4 ארומנטים;
+    app.use((err,req, res, next) => {
+        console.log("Second Stop: middleware with 4 arguments");
+        res.json("ok");
+    });
+
+    app.listen(3003, () => console.log("Listen on ports 3003"));
+};
+middlewareRecap3();
+
+
+
+
