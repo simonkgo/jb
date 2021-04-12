15dd189649c2a7f240c269648df70c791f683c43
Your Name <inbal@goldblatt.in>
2021-04-12 17:27:32 +0300

    lesson 8

--- nodejs-lesson-practice/server/product/product-entity.js
+++
@@ -1,9 +0,0 @@
-class Product {
-    constructor(obj) {
-        this.id = obj.id;
-        this.productName = obj.productName
-        this.status = obj.status;
-    };
-};
-
-module.exports = Product;
