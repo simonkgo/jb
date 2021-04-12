f0a0a44cbac171cb29dc0a7cf270a4cc42c57d23
simon <s@gmail.com>
2021-04-12 18:12:50 +0300

    nodejs: lesson 10 - start;

---
+++ nodejs-lesson-10/client/online-bank/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { LayoutComponent } from './Components/Layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutComponent/>
+    </div>
+  );
+}
+
+export default App;
